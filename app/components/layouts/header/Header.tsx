fd6978b02a13c1b0e7fb0c7d58e182a2846ead8d
Ayumu0922 <9bota.ayu6@yahoo.ne.jp>
2024-02-25 12:12:19 +0900

    initialize shadcn

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className=" divide-y border-gray-200 dark:border-b-gray-800 border-b">
+      <div className=" px-4 py-3 md:py-6 lg:px-6">
+        <div className=" flex items-center space-y-2 md:space-y-4 md:space-x-6">
+          <Link href="/" className=" text-2xl font-bold tracking-tighter mr-4">
+            Blog
+          </Link>
+          <nav className=" space-x-6">
+            <Link
+              href="/"
+              className=" font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className=" font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            >
+              About
+            </Link>
+            <Link
+              href="/bbs-posts/create"
+              className=" font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            >
+              Create Post
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
